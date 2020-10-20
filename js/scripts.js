070ffd3979b4680182decf874636f084c9143fb6
David Sterry <sterry.david@gmail.com>
2020-10-20 13:27:45 -0700

    fixed scripts.js to show cards
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $("h3.expand-text").click(function() {
-    $(".text-closed").toggle();
-    $(".text-open").toggle();
+  $(".expand-text1").click(function() {
+    $(".text-closed1").toggle();
+    $(".text-open1").toggle();
+  });
+  $(".expand-text2").click(function() {
+    $(".text-closed2").toggle();
+    $(".text-open2").toggle();
+  });
+  $(".expand-text3").click(function() {
+    $(".text-closed3").toggle();
+    $(".text-open3").toggle();
   });
 });
